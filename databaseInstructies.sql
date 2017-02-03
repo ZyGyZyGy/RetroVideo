ad3d2deef17da7ee419f008e940fdfd44c4d4a1d
Alex <jegorov.aleksandr@gmail.com>
2017-02-03 16:00:47 +0100

    SQL instrukties

--- databaseInstructies.sql
+++ databaseInstructies.sql
@@ -1,3 +1,4 @@
+-- SET SQL_SAFE_UPDATES = 0;
 select *
 from films;
 
@@ -42,22 +43,43 @@ update films
 set gereserveerd = gereserveerd + 1
 where id = ?;
 
+select klantid, filmid, reservatieDatum
+from reservaties
+where klantid = 1 and filmid = 7;
+
+
+
+-- RESERVATIES
+select films.titel, concat(voornaam, " ", familienaam) as naam
+from reservaties inner join klanten
+on reservaties.klantid = klanten.id
+inner join films
+on reservaties.filmid = films.id;
 
 select *
 from reservaties;
-
 
 delete from reservaties
 where klantid > 0;
 
 
-select *
+
+-- FILMS
+select id, titel, voorraad, gereserveerd
 from films;
 
+select *
+from films;
 
 update films
 set gereserveerd = 0
 where gereserveerd > 0;
+
+
+
+
+
+
 
 
 
